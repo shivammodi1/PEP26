f2734c8deccde35ca7c1154d42ba52c2e5a753cc
shivammodi1 <smodi9846@gmail.com>
2026-01-27 14:40:22 +0530

    day 4

--- Day04/Find Peak Element.cpp
+++ Day04/Find Peak Element.cpp
@@ -17,7 +17,7 @@ class Solution {
 
         int st=1;
         int end=n-2;
-        whikle(st<=end){
+        while(st<=end){
             int mid = st+(end-st)/2;
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 return mid;
