ca725b93a0ae1befd5a40d568c48c44e3d711c8a
shivammodi1 <smodi9846@gmail.com>
2026-02-02 09:39:00 +0530

    2d-array

---
+++ Day08/eachRowSum.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int row, col;
+    cin >> row >> col; 
+
+    vector<vector<int>> arr(row, vector<int>(col)); 
+    
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    int ans =0;
+    int index = -1;
+    
+    for(int i=0;i<row;i++){
+        int sum = 0;
+        for(int j=0;j<col;j++){
+            sum+=arr[i][j]
+        }
+        if(ans<sum){
+            ans=sum;
+            index=i;
+        }
+    }
+    
+    cout<<"Sum is: "<<ans<<"Index is: "<<index<<endl;
+    
+}
