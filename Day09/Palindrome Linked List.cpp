e30818c2b3f24395fa6c1c1f1c485bbb5adece2a
shivammodi1 <smodi9846@gmail.com>
2026-02-04 17:14:26 +0530

    Palindrome LL

--- Day09/Palindrome Linked List.cpp
+++ Day09/Palindrome Linked List.cpp
@@ -1,7 +1,71 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Node{
+    int data;
+    Node* next;
+    public:
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
 
-int main(){
+// Palindrome Linked List
+bool isPalindrome(Node *head)
+{
+    if(head==NULL){
+        return true;
+    }
+    if(head->next == NULL){
+        return true;
+    }
+    int c=0;
+    Node* curr = head;
+    while(curr){
+        c++;
+        curr=curr->next;
+    }
+
+    c/=2;
+
+    Node* prev = NULL;
+    curr=head;
+    while(c--){
+        prev = curr;
+        curr = curr->next;
+    }
+    pre->next = NULL;// this for seprating the LL into two parts 
+
+    // from here reversing second part 
+    prev = NULL;
+    Node* fur=NULL;
+    while(curr){
+        fur = curr->next;
+        curr->next = prev;
+        prev=curr;
+        curr=fur;
+    }
+    
+    Node* head1 = head;
+    Node* head2 = prev;
+
+    while(head1 && head2){
+        if(head1->data != head2->data){
+            return false;
+        }else{
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+    }
+    return true;
+}
+
+
+
+
+int main()
+{
+
     return 0;
 }
