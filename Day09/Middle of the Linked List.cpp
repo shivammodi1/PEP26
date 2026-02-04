284129e6fb43ee9f06c2b9e52edd26d4df4c7155
shivammodi1 <smodi9846@gmail.com>
2026-02-04 13:22:41 +0530

    solution of mid element

---
+++ Day09/Middle of the Linked List.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+    public:
+    Node(int val){
+        data = val;
+        next=NULL;
+    }
+};
+
+Node* middleNode(ListNode* head){
+    if(head==NULL){
+        return head;
+    }
+
+    if(head->next == NULL){
+        return head;
+    }
+
+    // using sow fast pointer
+    // slow will move 1x speed
+    // fast will move 2x speed
+    Node* slow = head;
+    Node* fast = head;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+
+}
+
+int main(){
+    return 0;
+}
