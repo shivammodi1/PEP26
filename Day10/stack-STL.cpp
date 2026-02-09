f062317303bb3f87f72baa50da7710506884f984
shivammodi1 <smodi9846@gmail.com>
2026-02-09 12:17:53 +0530

    STL stack

---
+++ Day10/stack-STL.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    stack<int>st;
+    // push operation
+    st.push(10);
+    st.push(11);
+    st.push(12);
+    st.push(13);
+    st.push(14);
+    st.push(15);
+    // pop operation
+    st.pop(); // it will remove element from end (LIFO)
+    // top 
+    st.top(); // it will return the top element of stack
+    // isEmpty
+    st.empty(); // it will return true if stack is empty    
+    // size
+    st.size(); // it will return the size of stack
+    return 0;
+}
