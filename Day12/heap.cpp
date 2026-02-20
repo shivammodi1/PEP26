49df63394a2e72146fd96abc2d2027dbfd547b81
shivammodi1 <smodi9846@gmail.com>
2026-02-20 14:54:52 +0530

    pq

--- Day12/heap.cpp
+++ Day12/heap.cpp
@@ -25,58 +25,94 @@ public:
                 break;
             }
         }
+    }
 
-        // insert a value into the heap
-        void insert(int value)
+    // insert a value into the heap
+    void insert(int value)
+    {
+        heap.push_back(value);
+        bubbleUp(heap.size() - 1);
+    }
+
+    // deletion in heap
+    // To delete from heap:
+    // 1. Replace the root node with last node in the heap
+    // 2. Remove the last node
+    // 3. Bubble down the new root node to its correct position
+    void bubbleDown(int index)
+    {
+
+        int size = heap.size();
+
+        while (index < size)
         {
-            heap.push_back(value);
-            bubbleUp(heap.size() - 1);
-        }
-
-        // deletion in heap
-        // To delete from heap:
-        // 1. Replace the root node with last node in the heap
-        // 2. Remove the last node
-        // 3. Bubble down the new root node to its correct position
-       
-        // bubble-down
-        void bubbleDown(int index){
             int largest = index;
-            int left = 2*index + 1;
-            int right = 2*index + 2;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
 
-            if (left < heap.size() && heap[left] > heap[largest])
+            if (left < size && heap[left] > heap[largest])
             {
                 largest = left;
             }
-
-            if (right < heap.size() && heap[right] > heap[largest])
+            if (right < size && heap[right] > heap[largest])
             {
                 largest = right;
             }
 
+            
             if (largest != index)
             {
                 swap(heap[index], heap[largest]);
-                bubbleDown(largest);
+                index = largest;
+            }
+            else
+            {
+                break;
             }
         }
-
-        // delete the root element
-        void deleteRoot()
-        {
-            if (heap.empty())
-                return;
-
-            // replace root with last element
-            heap[0] = heap.back();
-            // remove last element
-            heap.pop_back();
-            // bubble down the new root
-            bubbleDown(0);
-        }
-
-        }
-
     }
+
+    // delete the root element
+    void deleteRoot()
+    {
+        if (heap.empty())
+            return;
+
+        // replace root with last element
+        heap[0] = heap.back();
+        // remove last element
+        heap.pop_back();
+        // bubble down the new root
+        bubbleDown(0);
+    }
+
 };
+
+int main()
+{
+    MaxHeap maxHeap;
+
+    maxHeap.insert(10);
+    maxHeap.insert(20);
+    maxHeap.insert(5);
+    maxHeap.insert(15);
+    maxHeap.insert(30);
+
+    cout << "Max Heap: ";
+    for (int value : maxHeap.heap)
+    {
+        cout << value << " ";
+    }
+    cout << endl;
+
+    maxHeap.deleteRoot();
+
+    cout << "Max Heap after deleting root: ";
+    for (int value : maxHeap.heap)
+    {
+        cout << value << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
